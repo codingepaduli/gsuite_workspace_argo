8364a9089b61971a0d2be57380d32f7c2243e834
codingepaduli <padulodeveloper@protonmail.com>
2024-12-23 15:15:03 +0100

    Moving working table's name to avoid to update each script every time

--- _environment_working_tables.sh
+++ _environment_working_tables.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # To avoid changes to this file (already tracked in git)
-#   git update-index --assume-unchanged _environment.sh
+#   git update-index --assume-unchanged _environment_working_tables.sh
 #
 # To commit changes to this file (already tracked in git)
-#   git update-index --no-assume-unchanged _environment.sh
+#   git update-index --no-assume-unchanged _environment_working_tables.sh
 
 # The name of the working tables are here to avoid
 # to update each script every time
@@ -15,5 +15,14 @@ TABELLA_STUDENTI="studenti_argo_2024_11_18"
 # Tabella sezioni per anno
 TABELLA_SEZIONI="sezioni_2024_25"
 
+# Tabella gruppi
+TABELLA_GRUPPI="gruppi_2024_25"
+
 SQL_FILTRO_ANNI=" AND sz.cl IN (1, 2, 3, 4, 5) " 
 SQL_FILTRO_SEZIONI=" AND sz.addr_argo IN ('tr', 'en', 'in', 'm', 'od', 'idd', 'et', 'tlt', 'm_sirio', 'et_sirio') " 
+
+# Tabella personale versionata alla data indicata
+TABELLA_PERSONALE="personale_argo_2024_12_17"
+
+# Tabella in cui importo i CSV
+TABELLA_CSV="tabella_CSV"
