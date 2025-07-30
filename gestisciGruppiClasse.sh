96f2d2fc6e73bf1d2348672cd092481d62328ca9
codingepaduli <padulodeveloper@protonmail.com>
2025-07-30 10:33:58 +0200

    Apply difference on data to class groups, adding or removing modified students to classes

--- gestisciGruppiClasse.sh
+++ gestisciGruppiClasse.sh
@@ -138,9 +138,44 @@ main() {
                 " > "$EXPORT_DIR_DATE/studenti_per_classe_$CURRENT_DATE.csv"
                 ;;
             7)
-                echo "Gestisci differenza ..."
+                echo "Gestisci differenza tra tabella prima e dopo ..."
 
-                # $RUN_CMD_WITH_QUERY --command deleteUsers --group " NO " --query "select d.email from $TABELLA_CSV d WHERE d.email IS NOT NULL ORDER BY d.name;"
+                local QUERY_DIFF="
+                  
+                  --, sp.cl, sp.sez, sp.datar, sd.email_gsuite, sd.cl, sd.sez, szd.sezione_gsuite, sd.datar
+                  FROM $TABELLA_STUDENTI sp
+                    INNER JOIN $TABELLA_STUDENTI_DOPO sd 
+                    ON sp.email_gsuite = sd.email_gsuite
+                    INNER JOIN $TABELLA_SEZIONI szp 
+                    ON sp.sez = szp.sez_argo AND sp.cl = szp.cl 
+                    INNER JOIN $TABELLA_SEZIONI szd 
+                    ON sd.sez = szd.sez_argo AND sd.cl = szd.cl 
+                  WHERE sd.email_gsuite IS NOT NULL 
+                    AND sd.email_gsuite != ''  
+                    AND (sp.datar IS NULL OR sp.datar = '')
+                    AND (
+                      (sp.cl != sd.cl OR sp.sez != sd.sez)
+                    	OR (sp.datar = '' AND sd.datar != '')
+                    );
+                "
+
+                while IFS="," read -r email_gsuite sezione_gsuite; do
+
+                  echo "cancello $email_gsuite da classe $sezione_gsuite"
+                  $RUN_CMD_WITH_QUERY --command deleteMembersFromGroup --group "$sezione_gsuite" --query "SELECT '$email_gsuite' as email_gsuite;"
+
+                done < <($SQLITE_CMD -csv studenti.db "
+                  SELECT sp.email_gsuite, szp.sezione_gsuite $QUERY_DIFF
+                  " | sed "s/\"//g")
+                
+                while IFS="," read -r email_gsuite sezione_gsuite; do
+
+                  echo "inserisco $email_gsuite in classe $sezione_gsuite"
+                  $RUN_CMD_WITH_QUERY --command addMembersToGroup --group "$sezione_gsuite" --query "SELECT '$email_gsuite' as email_gsuite;"
+
+                done < <($SQLITE_CMD -csv studenti.db "
+                  SELECT sp.email_gsuite, szd.sezione_gsuite $QUERY_DIFF
+                  " | sed "s/\"//g")
                 ;;
             20)
                 echo "Arrivederci!"
