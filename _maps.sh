51eff77138ac73378308c91896d28f54e083eca8
codingepaduli <padulodeveloper@protonmail.com>
2025-08-01 12:39:48 +0200

    Add loggging functions

--- _maps.sh
+++ _maps.sh
@@ -66,6 +66,53 @@ checkAllVarsNotEmpty() {
 }
 export -f checkAllVarsNotEmpty
 
+function log::level_is_active {
+  local check_level current_level
+  check_level=$1
+
+  declare -A log_levels=(
+    [DEBUG]=1
+    [INFO]=2
+    [WARN]=3
+    [ERROR]=4
+  )
+
+  check_level="${log_levels["$check_level"]}"
+  current_level="${log_levels["$LOG_LEVEL"]}"
+
+  (( check_level >= current_level ))
+}
+
+function log::_write_log {
+  local timestamp file function_name log_level
+  log_level=$1
+  shift
+
+  if log::level_is_active "$log_level"; then
+    timestamp=$(date +'%y.%m.%d %H:%M:%S')
+    file="${BASH_SOURCE[2]##*/}"
+    function_name="${FUNCNAME[2]}"
+    >&2 printf '%s [%s] [%s - %s]: %s\n'  "$log_level" "$timestamp" "$file" "$function_name" "${*}"
+  fi
+
+  if [ "$log_level" == "ERROR" ]; then
+      log::printStackTrace
+  fi
+}
+
+log::printStackTrace() {
+    local stack_offset=2
+
+    for stack_id in "${!FUNCNAME[@]}"; do
+      if [[ "$stack_offset" -le "$stack_id" ]]; then
+        local source_file="${BASH_SOURCE[$stack_id]}"
+        local function="${FUNCNAME[$stack_id]}"
+        local line="${BASH_LINENO[$(( stack_id - 1 ))]}"
+        >&2 printf '\t%s:%s:%s\n' "$source_file" "$function" "$line"
+      fi
+    done
+}
+
 handle_error() {
     local environment="${DEPLOY_ENV:-}"
     local currentTime="$(date +'%Y-%m-%d %H:%M:%S')"
@@ -82,16 +129,7 @@ handle_error() {
         echo "[$currentUser][$environment] $currentTime ERROR in Script: $file"
         echo "  The command: '$cmd' returned exit status '$exit_code'"
         printf '%s:\n' '  Stacktrace:'
-        local stack_offset=1
-
-        for stack_id in "${!FUNCNAME[@]}"; do
-          if [[ "$stack_offset" -le "$stack_id" ]]; then
-            local source_file="${BASH_SOURCE[$stack_id]}"
-            local function="${FUNCNAME[$stack_id]}"
-            local line="${BASH_LINENO[$(( stack_id - 1 ))]}"
-            >&2 printf '\t%s:%s:%s\n' "$source_file" "$function" "$line"
-          fi
-        done
+        log::printStackTrace
     } >&2
 
     exit $exit_code
@@ -103,6 +141,12 @@ export -f handle_error
 # E: per gestire gli errori all'interno delle funzioni con trap.
 # u: Termina lo script se si tenta di utilizzare una variabile non definita
 # o pipefail: Imposta il valore di uscita di una pipeline (una serie di comandi collegati) per essere diverso da zero se uno qualsiasi dei comandi nella pipeline fallisce. Senza questa opzione, il valore di uscita della pipeline è quello dell'ultimo comando.
+
+LOG_LEVEL="DEBUG"
+
+# log::_write_log "DEBUG" "TEST debug"
+# log::_write_log "WARN" "TEST warn"
+# log::_write_log "ERROR" "ERRORE NON PREVISTO"
 
 set -eEuo pipefail
 
