46b8efd395df3ac206cdbe0b717bcfac8ce8b0a0
codingepaduli <padulodeveloper@protonmail.com>
2025-10-28 09:56:31 +0100

    Adds default values for environment variables

--- _environment.sh
+++ _environment.sh
@@ -67,19 +67,40 @@ EXPORT_DIR="$BASE_DIR/export"
 # Sotto-cartella di esportazione con data
 EXPORT_DIR_DATE="$EXPORT_DIR/export_$CURRENT_DATE"
 
-#########################################
-# Tables' name (for version management) #
-#########################################
+####################################
+# Tables' name related to students #
+####################################
 
 # Tabella studenti versionata alla data indicata
 TABELLA_STUDENTI="" # studenti_argo_2025_06_22
-TABELLA_STUDENTI_SERALE="" # temporanea studenti_argo_2025_06_22_sirio
+TABELLA_STUDENTI_SERALE="" # studenti_argo_2025_06_22_sirio
 
 # Usata nella gestione classi per rilevare le differenze
 TABELLA_STUDENTI_PRECEDENTE="" # studenti_argo_2025_06_01
 
+# Per operazioni in un determinato periodo
+PERIODO_STUDENTI_DA='2025-10-20'
+PERIODO_STUDENTI_A='2025-10-20'
+
+####################################
+# Tables' name related to sections #
+####################################
+
 # Tabella sezioni per anno
 TABELLA_SEZIONI="" # sezioni_2024_25
+
+# All classes and addresses enabled
+SQL_FILTRO_ANNI=" AND sz.cl IN (1, 2, 3, 4, 5) " 
+SQL_FILTRO_SEZIONI_DIURNO=" AND sz.addr_argo IN ('tr', 'en', 'in', 'm', 'mDD', 'od', 'idd', 'et', 'tlt') "
+SQL_FILTRO_SEZIONI_SERALE=" AND sz.addr_argo IN ('m_sirio', 'et_sirio', 'me_sirio') "
+SQL_FILTRO_SEZIONI=" $SQL_FILTRO_SEZIONI_DIURNO $SQL_FILTRO_SEZIONI_SERALE "
+
+###########################################
+# Tables' name related to students' group #
+###########################################
+
+# Tabella in cui importo gli account GSuite relativi agli studenti
+TABELLA_STUDENTI_GSUITE="" # studenti_gsuite_2025_08_21
 
 # Tabella gruppi
 TABELLA_GRUPPI="" # gruppi_2024_25
@@ -87,37 +108,49 @@ TABELLA_GRUPPI="" # gruppi_2024_25
 # Gruppi GSuite addizionali (non in sezioni)
 GSUITE_ADDITIONAL_GROUPS=("")
 
-# All classes and addresses enabled
-SQL_FILTRO_ANNI=" AND sz.cl IN (1, 2, 3, 4, 5) " 
-SQL_FILTRO_SEZIONI=" AND sz.addr_argo IN ('tr', 'en', 'in', 'm', 'od', 'idd', 'et', 'tlt', 'm_sirio', 'et_sirio') "
+#####################################
+# Tables' name related to employees #
+#####################################
+
+# Tabella personale versionata alla data indicata
+TABELLA_PERSONALE="" # personale_argo_2025_10_20
 
 # Per operazioni in un determinato periodo
-PERIODO_STUDENTI_DA='2025-07-15'
-PERIODO_STUDENTI_A='2025-07-15'
+PERIODO_PERSONALE_DA='2025-10-20'
+PERIODO_PERSONALE_A='2025-10-20'
 
-# Tabella personale
-TABELLA_PERSONALE="" # personale_argo_2025_01_22
+# Tabella personale precedente per confronto
+TABELLA_PERSONALE_PRECEDENTE="personale_argo_2025_09_23"
 
-# Tabella CdC
-TABELLA_CDC_ARGO="" # Cdc_2025_01_13
+#####################################
+# Tables' name for gSuite users     #
+#####################################
 
-# Tabella account GSuite studenti
-TABELLA_STUDENTI_GSUITE="" # studenti_gsuite_20241201
-
-# Tabella account GSuite docenti
-TABELLA_DOCENTI_GSUITE="" # docenti_gsuite
-
-# Tabella in cui importo le risposte di Google Moduli da CSV
-TABELLA_GMODULI="" # GModuli
-
-# Tabella dipartimenti
-TABELLA_DIPARTIMENTI="" # gsuite_dipartimenti
+TABELLA_UTENTI_GSUITE='tutti_2025_10_20'
 
 # unità organizzative gSuite
 GSUITE_OU_DOCENTI="DOCENTI"
 GSUITE_OU_ATA="ATA"
 
-# Tabella account Wordpress
+#####################################
+# Tables' name for CdC              #
+#####################################
+
+# Tabella CdC versionata alla data indicata
+TABELLA_CDC_ARGO="Cdc_2025_01_13"
+
+#####################################
+# Tables' name for Moduli           #
+#####################################
+
+# Tabella in cui importo le risposte di Google Moduli da CSV
+TABELLA_GMODULI="" # GModuli
+
+#####################################
+# Tables' name for wordpress users  #
+#####################################
+
+# Tabella in cui importo gli account Wordpress
 TABELLA_UTENTI_WORDPRESS="" # wordpress_20250108
 
 # La ricerca di utenti su wordpress è paginata, 
@@ -137,3 +170,4 @@ WORDPRESS_USER_ID_FOR_DELETING="20"
 # Ruoli Wordpress
 WORDPRESS_ROLE_TEACHER="docente"
 WORDPRESS_ROLE_ATA="personale_ata"
+
