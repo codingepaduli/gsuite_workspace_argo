88d7e60126ba37f87cbfb818f342f909cc18605d
codingepaduli <padulodeveloper@protonmail.com>
2026-02-23 12:44:19 +0100

    Automating multiple operations on students

---
+++ gestioneCompleta.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+# shellcheck source=./_environment.sh
+source "./_environment.sh"
+source "./_environment_working_tables.sh"
+source "./_maps.sh"
+
+SQL_QUERY_SEZIONI="SELECT sz.sezione_gsuite FROM $TABELLA_SEZIONI sz WHERE 1=1 $SQL_FILTRO_ANNI $SQL_FILTRO_SEZIONI ORDER BY sz.sezione_gsuite"
+
+# Crea la query per gruppi GSUITE aggiuntivi, indicati nel file di configurazione
+SQL_QUERY_ADDITIONAL_GROUPS="WITH temp AS ( SELECT NULL AS value "
+# Itera sull'array
+for value in "${GSUITE_ADDITIONAL_GROUPS[@]}"; do
+    # Aggiungi il valore alla lista, racchiudendolo tra apici
+    SQL_QUERY_ADDITIONAL_GROUPS+=" UNION ALL"
+    SQL_QUERY_ADDITIONAL_GROUPS+=" SELECT '$value' AS value"
+done
+SQL_QUERY_ADDITIONAL_GROUPS+=") SELECT value FROM temp WHERE value IS NOT NULL "
+
+# Funzione per mostrare il menu
+show_menu() {
+    echo "Gestione completa"
+    echo "-------------"
+    echo "Esecuzione in DRY-RUN mode: $dryRunFlag"
+    echo "-------------"
+    echo "0. Creo le tabelle, importo gli studenti"
+    echo "1. Eseguo script aggiornamento email"
+    echo "2. Creo le email e i relativi account su GSuite, li esporto in CSV"
+    echo "3. Aggiungi i nuovi studenti alle classi, effettua gli spostamenti, toglie i ritirati"
+
+    echo "20. Esci"
+}
+
+# Funzione principale
+main() {
+
+  while true; do
+    show_menu
+    read -p "Scegli un'opzione (1-20): " -r choice
+        
+    case $choice in
+      0)
+        echo "Creo le tabelle, importo gli studenti"
+
+        local CREATE_TABLE_STUDENTS=0
+        local IMPORT_STUDENTS=1
+        local CREATE_TABLE_STUDENTS_SIRIO=12
+        local IMPORT_STUDENTS_SIRIO=13
+        local COPY_STUDENTS_FROM_SIRIO=14
+
+        ./gestisciStudenti.sh "$CREATE_TABLE_STUDENTS"
+        read -p "Premi per continuare " -r _
+        ./gestisciStudenti.sh "$IMPORT_STUDENTS"
+        read -p "Premi per continuare " -r _
+        ./gestisciStudenti.sh "$CREATE_TABLE_STUDENTS_SIRIO"
+        read -p "Premi per continuare " -r _
+        ./gestisciStudenti.sh "$IMPORT_STUDENTS_SIRIO"
+        read -p "Premi per continuare " -r _
+        ./gestisciStudenti.sh "$COPY_STUDENTS_FROM_SIRIO"
+        read -p "Premi per continuare " -r _
+      ;;
+      1)
+        local MOVE_SCRIPT_OLD_STUDENTS=10
+        local CHECK_STUDENTS=16
+
+        ./gestisciStudenti.sh "$MOVE_SCRIPT_OLD_STUDENTS"
+        read -p "Premi per continuare " -r _
+
+        ./gestisciStudenti.sh "$CHECK_STUDENTS"
+        read -p "Premi per continuare " -r _
+      ;;
+      2)
+        echo "Creo le email e i relativi account su GSuite, li esporto in CSV"
+
+        local SHOW_NEW_STUDENTS=3
+        local CREATE_MAIL_STUDENTS=4
+        local EXPORT_NEW_STUDENTS=5
+        local CREATE_GSUITE_ACCOUNT_STUDENTS=6
+        local CREATE_SCRIPT_CF_STUDENTS=7
+        
+
+        ./gestisciStudenti.sh "$SHOW_NEW_STUDENTS"
+        ./gestisciStudenti.sh "$CREATE_MAIL_STUDENTS"
+        ./gestisciStudenti.sh "$SHOW_NEW_STUDENTS"
+        ./gestisciStudenti.sh "$EXPORT_NEW_STUDENTS" # Also by years
+        ./gestisciStudenti.sh "$CREATE_GSUITE_ACCOUNT_STUDENTS"
+        ./gestisciStudenti.sh "$CREATE_SCRIPT_CF_STUDENTS"
+      ;;
+      3)
+        echo "Aggiungi i nuovi studenti alle classi, effettua gli spostamenti, toglie i ritirati"
+
+        local SMOVE_STUDENTS_IN_CLASSES=8
+        local ADD_NEW_STUDENTS_IN_CLASSES=9
+        
+        ./gestisciGruppiClasse.sh "$SMOVE_STUDENTS_IN_CLASSES"
+        ./gestisciGruppiClasse.sh "$ADD_NEW_STUDENTS_IN_CLASSES"
+      ;;
+      20)
+        echo "Arrivederci!"
+        exit 0
+      ;;
+      *)
+        echo "Opzione non valida. Per favore, scegli un numero tra 1 e 20."
+        sleep 1
+      ;;
+    esac
+
+    # Pausa per permettere all'utente di leggere il risultato
+    read -p "Premi Invio per continuare..." -r _
+  done
+}
+
+showConfig() {
+  if log::level_is_active "CONFIG"; then
+    log::_write_log "CONFIG" "Checking config - $(date --date='today' '+%Y-%m-%d %H:%M:%S')"
+    log::_write_log "CONFIG" "-----------------------------------------"
+    log::_write_log "CONFIG" "Current date: $CURRENT_DATE"
+    log::_write_log "CONFIG" "Tabella studenti diurno: $TABELLA_STUDENTI"
+    log::_write_log "CONFIG" "Tabella studenti precedente per confronto: $TABELLA_STUDENTI_PRECEDENTE"
+    log::_write_log "CONFIG" "Tabella sezioni: $TABELLA_SEZIONI"
+    log::_write_log "CONFIG" "Inizio periodo (compreso): $PERIODO_STUDENTI_DA" 
+    log::_write_log "CONFIG" "Fine periodo (compreso): $PERIODO_STUDENTI_A"
+    log::_write_log "CONFIG" "Cartella di esportazione: $EXPORT_DIR_DATE"
+    log::_write_log "CONFIG" "Query sezioni: $SQL_QUERY_SEZIONI"
+    log::_write_log "CONFIG" "Query altri gruppi: $SQL_QUERY_ADDITIONAL_GROUPS"
+    log::_write_log "CONFIG" "-----------------------------------------"
+    read -p "Premi Invio per continuare..." -r _
+  fi
+}
+
+# Show config vars
+showConfig
+
+# Avvia la funzione principale
+main
+
