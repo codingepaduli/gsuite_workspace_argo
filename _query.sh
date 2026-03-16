7bdee8d521175c97431aeadc1ba837427d2f86a9
codingepaduli <padulodeveloper@protonmail.com>
2026-03-16 18:38:50 +0100

    improving query for classes

--- _query.sh
+++ _query.sh
@@ -34,13 +34,13 @@ declare -A sectionQueryParam=(
 function query::getQuerySezioni {
     local FIELDS="${1:-${sectionQueryParam[FIELDS]}}"
     local ORDERING="${2:-${sectionQueryParam[ORDERING]}}"
-    local FILTER_YEARS_ON="${3:-${sectionQueryParam[FILTER_YEARS_ON]}}"
+    local FILTER_YEARS_FLAG="${3:-${sectionQueryParam[FILTER_YEARS_ON]}}"
     local FILTER_YEARS="${4:-${sectionQueryParam[FILTER_YEARS]}}"
-    local FILTER_ADDRESS_ARGO_ON="${5:-${sectionQueryParam[FILTER_ADDRESS_ARGO_ON]}}"
+    local FILTER_ADDRESS_ARGO_FLAG="${5:-${sectionQueryParam[FILTER_ADDRESS_ARGO_ON]}}"
     local FILTER_ADDRESS_ARGO="${6:-${sectionQueryParam[FILTER_ADDRESS_ARGO]}}"
-    local FILTER_ADDRESS_GSUITE_ON="${7:-${sectionQueryParam[FILTER_ADDRESS_GSUITE_OFF]}}"
+    local FILTER_ADDRESS_GSUITE_FLAG="${7:-${sectionQueryParam[FILTER_ADDRESS_GSUITE_OFF]}}"
     local FILTER_ADDRESS_GSUITE="${8:-${sectionQueryParam[FILTER_ADDRESS_GSUITE]}}"
-    local FILTER_CLASSES_ON="${9:-${sectionQueryParam[FILTER_CLASSES_OFF]}}"
+    local FILTER_CLASSES_FLAG="${9:-${sectionQueryParam[FILTER_CLASSES_OFF]}}"
     local FILTER_CLASSES="${10:-${sectionQueryParam[FILTER_CLASSES]}}"
     local FILTER_SUPERVISORS_EXISTS="${11:-${sectionQueryParam[FILTER_SUPERVISORS_EXISTS_OFF]}}"
     local FILTER_SUPERVISORS_NOT_EXISTS="${12:-${sectionQueryParam[FILTER_SUPERVISORS_NOT_EXISTS_OFF]}}"
@@ -49,10 +49,10 @@ function query::getQuerySezioni {
           SELECT $FIELDS 
           FROM $TABELLA_SEZIONI
           WHERE 1=1 
-            AND (1=$FILTER_YEARS_ON OR cl IN ( $FILTER_YEARS ) )
-            AND (1=$FILTER_ADDRESS_ARGO_ON OR addr_argo IN ( $FILTER_ADDRESS_ARGO ) )
-            AND (1=$FILTER_ADDRESS_GSUITE_ON OR addr_gsuite IN ( $FILTER_ADDRESS_GSUITE ) )
-            AND (1=$FILTER_CLASSES_ON OR sezione_gsuite IN ( $FILTER_CLASSES ) )
+            AND (1=$FILTER_YEARS_FLAG OR cl IN ( $FILTER_YEARS ) )
+            AND (1=$FILTER_ADDRESS_ARGO_FLAG OR addr_argo IN ( $FILTER_ADDRESS_ARGO ) )
+            AND (1=$FILTER_ADDRESS_GSUITE_FLAG OR addr_gsuite IN ( $FILTER_ADDRESS_GSUITE ) )
+            AND (1=$FILTER_CLASSES_FLAG OR sezione_gsuite IN ( $FILTER_CLASSES ) )
             AND (1=$FILTER_SUPERVISORS_EXISTS OR 
                   ( email_coordinatore IS NOT NULL AND LOWER( email_coordinatore) != '' )
                 )
@@ -75,7 +75,24 @@ function query::getQuerySezioniSupervisorNotEmpty {
   local FIELDS="${1:-${sectionQueryParam[FIELDS]}}"
   local ORDERING="${2:-${sectionQueryParam[ORDERING]}}"
 
-  query=$(query::getQuerySezioni "$FIELDS" "$ORDERING" "${sectionQueryParam[FILTER_YEARS_OFF]}" " "  "${sectionQueryParam[FILTER_ADDRESS_ARGO_OFF]}" " " "${sectionQueryParam[FILTER_ADDRESS_GSUITE_OFF]}" " " "${sectionQueryParam[FILTER_CLASSES_OFF]}" " " "${sectionQueryParam[FILTER_SUPERVISORS_EXISTS_ON]}" "${sectionQueryParam[FILTER_SUPERVISORS_NOT_EXISTS_OFF]}" )
+  # Dichiarazione di un array associativo per i parametri
+  declare cliParam=()
+
+  # Imposta i valori per le chiavi specifiche
+  cliParam[1]="${1:-${sectionQueryParam[FIELDS]}}"
+  cliParam[2]="${2:-${sectionQueryParam[ORDERING]}}"
+  cliParam[3]="${sectionQueryParam[FILTER_YEARS_OFF]}"
+  cliParam[4]=" "
+  cliParam[5]="${sectionQueryParam[FILTER_ADDRESS_ARGO_OFF]}"
+  cliParam[6]=" "
+  cliParam[7]="${sectionQueryParam[FILTER_ADDRESS_GSUITE_OFF]}"
+  cliParam[8]=" "
+  cliParam[9]="${sectionQueryParam[FILTER_CLASSES_OFF]}"
+  cliParam[10]=" "
+  cliParam[11]="${sectionQueryParam[FILTER_SUPERVISORS_EXISTS_ON]}"
+  cliParam[12]="${sectionQueryParam[FILTER_SUPERVISORS_NOT_EXISTS_OFF]}"
+
+  query=$(query::getQuerySezioni "${cliParam[@]}" )
   echo "$query"
 }
 
