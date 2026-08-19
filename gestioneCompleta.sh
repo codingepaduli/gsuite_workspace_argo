f1e56574a6bed07b82dd9b10702f63fb9ffafc62
codingepaduli <padulodeveloper@protonmail.com>
2026-08-19 14:04:41 +0200

    Automating sending email to cancelled students

--- gestioneCompleta.sh
+++ gestioneCompleta.sh
@@ -5,18 +5,6 @@ source "./_environment.sh"
 source "./_environment_working_tables.sh"
 source "./_maps.sh"
 
-SQL_QUERY_SEZIONI="SELECT sz.sezione_gsuite FROM $TABELLA_SEZIONI sz WHERE 1=1 AND sz.cl IN ( $SQL_FILTRO_ANNI ) AND sz.addr_argo IN ( $SQL_FILTRO_SEZIONI ) ORDER BY sz.sezione_gsuite"
-
-# Crea la query per gruppi GSUITE aggiuntivi, indicati nel file di configurazione
-SQL_QUERY_ADDITIONAL_GROUPS="WITH temp AS ( SELECT NULL AS value "
-# Itera sull'array
-for value in "${GSUITE_ADDITIONAL_GROUPS[@]}"; do
-    # Aggiungi il valore alla lista, racchiudendolo tra apici
-    SQL_QUERY_ADDITIONAL_GROUPS+=" UNION ALL"
-    SQL_QUERY_ADDITIONAL_GROUPS+=" SELECT '$value' AS value"
-done
-SQL_QUERY_ADDITIONAL_GROUPS+=") SELECT value FROM temp WHERE value IS NOT NULL "
-
 # Funzione per mostrare il menu
 show_menu() {
     echo "Gestione completa"
@@ -26,7 +14,7 @@ show_menu() {
     echo "0. Creo le tabelle, importo gli studenti"
     echo "1. Eseguo script aggiornamento email studenti"
     echo "2. Creo le email studenti e i relativi account su GSuite, li esporto in CSV"
-    echo "3. Aggiungi i nuovi studenti alle classi, effettua gli spostamenti, toglie i ritirati"
+    echo "3. Aggiungi i nuovi studenti alle classi, effettua gli spostamenti, toglie i ritirati e invia loro una mail"
 
     echo "5. Invio singola email ad ogni coordinatore con elenco studenti per classe"
 
@@ -70,12 +58,12 @@ main() {
       ;;
       1)
         local CREATE_SCRIPT_CF_STUDENTS=7
-        local MOVE_SCRIPT_OLD_STUDENTS=10
+        local MOVE_AND_RUN_SCRIPT_OLD_STUDENTS=10
         local CHECK_STUDENTS_CF=16
         local CHECK_STUDENTS_GSUITE=17
 
         ./gestisciStudenti.sh "$CREATE_SCRIPT_CF_STUDENTS"
-        ./gestisciStudenti.sh "$MOVE_SCRIPT_OLD_STUDENTS"
+        ./gestisciStudenti.sh "$MOVE_AND_RUN_SCRIPT_OLD_STUDENTS"
         ./gestisciStudenti.sh "$CHECK_STUDENTS_CF"
         ./gestisciStudenti.sh "$CHECK_STUDENTS_GSUITE"
       ;;
@@ -96,13 +84,19 @@ main() {
         ./gestisciStudenti.sh "$CREATE_SCRIPT_CF_STUDENTS"
       ;;
       3)
-        echo "Aggiungi i nuovi studenti alle classi, effettua gli spostamenti, toglie i ritirati"
+        echo "Aggiungi i nuovi studenti alle classi, effettua gli spostamenti, toglie i ritirati e invia loro una mail"
 
         local SMOVE_STUDENTS_IN_CLASSES=8
         local ADD_NEW_STUDENTS_IN_CLASSES=9
+        local SHOW_DELETED_STUDENTS=11
+        local SEND_MAIL_TO_DELETED_STUDENTS=15
         
         ./gestisciGruppiClasse.sh "$SMOVE_STUDENTS_IN_CLASSES"
         ./gestisciGruppiClasse.sh "$ADD_NEW_STUDENTS_IN_CLASSES"
+
+        echo "Invio email ai ritirati..."
+        ./gestisciStudenti.sh "$SHOW_DELETED_STUDENTS"
+        ./gestisciStudenti.sh "$SEND_MAIL_TO_DELETED_STUDENTS"
       ;;
       5)
         echo "5. Invio singola email ad ogni coordinatore con elenco studenti per classe"
@@ -207,8 +201,6 @@ showConfig() {
     log::_write_log "CONFIG" "Inizio periodo (compreso): $PERIODO_STUDENTI_DA" 
     log::_write_log "CONFIG" "Fine periodo (compreso): $PERIODO_STUDENTI_A"
     log::_write_log "CONFIG" "Cartella di esportazione: $EXPORT_DIR_DATE"
-    log::_write_log "CONFIG" "Query sezioni: $SQL_QUERY_SEZIONI"
-    log::_write_log "CONFIG" "Query altri gruppi: $SQL_QUERY_ADDITIONAL_GROUPS"
     log::_write_log "CONFIG" "-----------------------------------------"
     read -p "Premi Invio per continuare..." -r _
   fi
