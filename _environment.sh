72f7e80a08d83bc241f6054e7d0b8aaa479cff3e
codingepaduli <padulodeveloper@protonmail.com>
2026-09-07 23:00:51 +0200

    Adds default values for environment variables

--- _environment.sh
+++ _environment.sh
@@ -114,7 +114,7 @@ TABELLA_SEZIONI="" # sezioni_2024_25
 
 # All classes and addresses enabled
 SQL_FILTRO_ANNI=" 1, 2, 3, 4, 5 " 
-SQL_SEZIONI_DIURNO=" 'tr', 'en', 'in', 'm', 'mDD', 'od', 'idd', 'et', 'tlt' "
+SQL_SEZIONI_DIURNO=" 'tr', 'en', 'in', 'm', 'mDD', 'od', 'idd', 'et', 'tlt', 'Qi' "
 SQL_SEZIONI_SERALE=" 'm_sirio', 'et_sirio', 'me_sirio' "
 
 # GSuite data are UPPERCASE
